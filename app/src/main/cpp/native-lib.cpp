943a2c71ca6036d84b19162af228e9f4926abf7c
Jasmin Plappert <jasmin.plappert@gmx.de>
2018-11-13 15:54:07 +0100

    first steps with kotlin

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,6 @@
 #include <jni.h>
 #include <string>
+#include <iostream>
 
 extern "C" JNIEXPORT jstring
 
--- app/src/main/java/com/j/opencvapp/MainActivity.kt
+++ app/src/main/java/com/j/opencvapp/MainActivity.kt
@@ -2,12 +2,20 @@ package com.j.opencvapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val button_dynamic = Button(this)
+        button_dynamic.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        button_dynamic.text = "Dynamic Button"
         setContentView(R.layout.activity_main)
 
         // Example of a call to a native method
